e5a7b6a9a9e7c2a5862b306206394fce4280e706
Nugra21 <ludang400060@gmail.com>
2025-11-26 16:00:08 +0700

    -

--- lib/config.dart
+++ lib/config.dart
@@ -1,4 +1,4 @@
-const String baseUrl = "https://ludang.wuaze.com/";
+const String baseUrl = "http://192.168.0.102/backendapk";
 // contoh: http://192.168.1.5/login_smk/ kalau pakai device fisik
 //
 // 
