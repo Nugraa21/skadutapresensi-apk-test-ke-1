288ea8a7a6f1014d4d68eda5eee58b802f11a460
Nugra21 <ludang400060@gmail.com>
2025-11-26 09:00:30 +0700

    backup versi android

---
+++ backup/android backup/b1/android/app/build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "com.example.skadutapresensi"
+    compileSdk = flutter.compileSdkVersion
+
+    // FIX: Pakai NDK versi yang diminta plugin (27.0.12077973)
+    ndkVersion = "27.0.12077973"
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    defaultConfig {
+        applicationId = "com.example.skadutapresensi"
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
---
+++ backup/android backup/b1/android/app/src/main/kotlin/com/example/skadutapresensi/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.skadutapresensi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
