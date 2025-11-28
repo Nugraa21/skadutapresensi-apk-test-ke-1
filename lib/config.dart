e49aa3cfae8982aaac9dee4bed52185785a3a37e
Nugra21 <ludang400060@gmail.com>
2025-11-28 08:06:31 +0700

    merubah api

--- lib/config.dart
+++ lib/config.dart
@@ -1,5 +1,5 @@
-// const String baseUrl = "http://192.168.0.103/backendapk"; // gudang barat
-const String baseUrl = "http://192.168.137.1/backendapk"; // smk yk
+const String baseUrl = "http://192.168.0.105/backendapk/"; // gudang barat
+// const String baseUrl = "http://192.168.137.1/backendapk"; // smk yk
 
 
 // contoh: http://192.168.1.5/login_smk/ kalau pakai device fisik
