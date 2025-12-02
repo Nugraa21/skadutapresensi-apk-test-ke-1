11bac2f162816050778da1059e54bf389836107d
Nugra21 <ludang400060@gmail.com>
2025-12-02 17:53:16 +0700

    -

--- lib/config.dart
+++ lib/config.dart
@@ -3,7 +3,7 @@ const String baseUrl = "http://10.10.70.255/backendapk/"; // gudang barat
 // const String baseUrl = "http://192.168.137.1/backendapk"; // smk yk
 // const String baseUrl = "http://10.10.70.255/backendapk"; // smk yk
 
-
+  
 // contoh: http://192.168.1.5/login_smk/ kalau pakai device fisik
 // contoh: http://10.10.70.255/login_smk/ kalau pakai device fisik
 //
