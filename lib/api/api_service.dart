4ea0c057a5a16f6e5a4636b26aad3648b22302c5
Nugra21 <ludang400060@gmail.com>
2025-12-02 17:17:10 +0700

    -

--- lib/api/api_service.dart
+++ lib/api/api_service.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 
 class ApiService {
   static const String baseUrl =
-      "https://pseudosiphonal-tisha-unenvenomed.ngrok-free.dev/backendapk/"; // api ngrok dan harus di ganti saat restart server
+      "http://10.10.70.255/backendapk/"; // api ngrok dan harus di ganti saat restart server
   // LOGIN
   static Future<Map<String, dynamic>> login({
     required String input,
--- lib/config.dart
+++ lib/config.dart
@@ -1,10 +1,11 @@
 // const String baseUrl = "http://192.168.0.105/backendapk/"; // gudang barat
-const String baseUrl =
-    "https://pseudosiphonal-tisha-unenvenomed.ngrok-free.dev/backendapk/"; // gudang barat
+const String baseUrl = "http://10.10.70.255/backendapk/"; // gudang barat
 // const String baseUrl = "http://192.168.137.1/backendapk"; // smk yk
+// const String baseUrl = "http://10.10.70.255/backendapk"; // smk yk
 
 
 // contoh: http://192.168.1.5/login_smk/ kalau pakai device fisik
+// contoh: http://10.10.70.255/login_smk/ kalau pakai device fisik
 //
 // 
 //
